1a08ee377363f8ef3f277239da4d754cc914331e
ahmedofweb <abdulxamidweb@gmail.com>
2023-07-13 01:37:35 +0300

    news

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -8,16 +8,40 @@ function Portfolio() {
   const [propsLink, setPropsLink] = useState(null)
   const [portfolio, setPortfolio] = useState([
     {
-        img: './bookuz.jpg',
-        link: '',
-        id: 2
+      img: './weather.jpg',
+      link: 'https://aa-the-weather.netlify.app/',
+      github: '',
+      id: 7
     },
     {
-        img: './countries.jpg',
-        link: 'https://countries-info-at-react.netlify.app/',
-        github: 'https://github.com/ahmedofweb/countries-info-at-React',
-        id: 3
+      img: './countriesFull.jpg',
+      link: 'https://aa-country-info.netlify.app/',
+      github: '',
+      id: 5
     },
+    {
+      img: './randomUser.jpg',
+      link: 'https://aa-random-user.netlify.app/',
+      github: '',
+      id: 6
+    },  
+    {
+      img: './cuser.jpg',
+      link: 'http://createusers1.netlify.app/',
+      github: 'https://github.com/ahmedofweb/CreateUser',
+      id: 1
+    },  
+    {
+      img: './bookuz.jpg',
+      link: '',
+      id: 2
+    },
+    {
+      img: './infoWords.jpg',
+      link: 'https://words-info.netlify.app/',
+      github: '',
+      id: 8
+    }, 
     {
         img: './wordsplay.jpg',
         link: 'https://ia-game.netlify.app/',
@@ -25,13 +49,18 @@ function Portfolio() {
         id: 4
     }, 
     {
-      img: './cuser.jpg',
-      link: 'http://createusers1.netlify.app/',
-      github: 'https://github.com/ahmedofweb/CreateUser',
-      id: 1
+      img: './countries.jpg',
+      link: 'https://countries-info-at-react.netlify.app/',
+      github: 'https://github.com/ahmedofweb/countries-info-at-React',
+      id: 3
+    },
+    {
+      img: './recipe.jpg',
+      link: 'https://1recipes.netlify.app/',
+      github: '',
+      id: 9
     }
 ])
-
 
 return(
   <div className='portfolio'>
